4217b39660faf0e892452e15642dbbcd234bd545
Spotlight0xff <spotlight0xff@gmail.com>
2015-02-03 23:14:46 +0100

    works now, getStops() returns dict

--- aseag/__init__.py
+++ aseag/__init__.py
@@ -6,16 +6,22 @@ def getStops():
     payload = {'ReturnList': 'StopPointName,StopID'}
     url = 'http://ivu.aseag.de/interfaces/ura/instant_V1'
     r = requests.get(url, params=payload)
+    d = {}
     if not r.ok:
         return False
     content = r.content
     for line in content.splitlines():
         line = line.decode()  # deocode to text
-        match = re.findall('\[(.*),(.*),(.*)\]', line)[0]
-        stopId = match[2]
-        stopId = int(stopId.split('"')[1])
-        stopName = match[1]
-        print(stopName+": "+stopId)
+        print(line)
+        match = re.findall('\[0,\"(.*)\",\"([0-9]*)\"\]', line)
+        if not match:
+#            print("fail["+str(len(match))+"]: " + line)
+            continue
+        match = match[0]
+        stopId = int(match[1])
+        stopName = match[0]
+        d[stopId] = stopName
+    return d
 
 
 def getRequest():
